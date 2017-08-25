78a341b177ad5d7754c42e8040fd2b17b2995557
Arthur Mota <motaarthur13@gmail.com>
2017-08-25 21:16:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Arthur"
+  end
+
+  get '/hometown' do
+    "My hometown is Patos de Minas"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is When I'm Gone"
+  end
 end
